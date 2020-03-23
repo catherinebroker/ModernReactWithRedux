70fd44a48f9161e7529b387952f8f56783283147
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-03-23 18:59:31 +0900

    #29 completed

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -12,7 +12,7 @@ const App = () => {
 
   return (
     <div>
-      <label className="label" htmlFor="name">Enter name:</label>
+      <label className="label" htmlor="name">Enter name:</label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
         {buttonText.text}
