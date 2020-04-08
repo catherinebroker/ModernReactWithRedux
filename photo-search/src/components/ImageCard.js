b8ba63c20063fb9774e5cfc06abb7e3fe412d738
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-04-08 12:01:22 +0900

    Section 10 (#108) Complete

--- photo-search/src/components/ImageCard.js
+++ photo-search/src/components/ImageCard.js
@@ -3,21 +3,28 @@ import React from 'react';
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
 
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef.current);
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
 
-    console.log(this.imageRef.current.clientHeight);
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+    console.log(spans);
+    this.setState({ spans: spans });
   }
 
   render() {
     const { alt_description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
       </div>
     )
