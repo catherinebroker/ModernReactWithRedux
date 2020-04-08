a38f24ce3a792b33b633bd2bce29ff75c0b54d67
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-04-08 15:58:36 +0900

    #114: Make SearchBar

--- video-search/src/components/SearchBar.js
+++ video-search/src/components/SearchBar.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    // TODO: Make sure to call callback from parent function
+  }
+
   render() {
     return (
-      <div>
-        SearchBar
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit} >
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
       </div>
     );
   }
