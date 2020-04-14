c2105effd3c1815b689032fc95381a819376bcb7
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-04-14 17:30:04 +0900

    #150

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,9 +1,11 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
     { title: 'Macarena', duration: '2:30' },
     { title: 'All Star', duration: '3:15' },
-    { title: 'I Want It That Way', duration: '4:20' }
+    { title: 'I Want It That Way', duration: '1:45' }
   ]
 };
 
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
