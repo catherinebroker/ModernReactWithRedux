f4175f7638b219cb996c5fcbf0d5defe578f3c8a
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-04-14 17:50:39 +0900

    #152

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
+
+
     return (
       <div>
         SongList
@@ -11,4 +14,8 @@ class SongList extends Component {
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
