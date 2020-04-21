1349dafa22039ae22b38e3b0e42127a6b774a70c
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-04-21 15:10:17 +0900

    #190

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
 export const fetchPosts = () => async dispatch => {
@@ -6,8 +7,11 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-export const fetchUser = (id) => async dispatch => {
-  const response = await jsonPlaceHolder.get(`/users/${id}`);
+export const fetchUser = function (id) {
+  return _.memoize(async function (dispatch) {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
 
-  dispatch({ type: 'FETCH_USER', payload: response.data })
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+  });
 };
+
