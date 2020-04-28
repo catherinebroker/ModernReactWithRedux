9fd8c802c72b560715748fb93ac0148daa74413a
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-04-28 16:44:33 +0900

    #202

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>PageOne</div>
+}
+
+const PageTwo = () => {
+  return <div>PageTwo
+    <button>
+      Click
+    </button>
+  </div>
+}
 
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" exact component={PageTwo} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
