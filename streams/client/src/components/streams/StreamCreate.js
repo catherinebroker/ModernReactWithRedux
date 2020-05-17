8d586b8f9ed5941fae50005d4d72b4f48a8b7bb9
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-05-17 11:43:10 +0900

    #237

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -18,12 +18,32 @@ class StreamCreate extends React.Component {
   render() {
     return (
       <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <Field
+          name="title"
+          component={this.renderInput}
+          label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description" />
         <button className="ui button pink">Submit</button>
       </form>
     );
   }
+}
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
 }
 
 
