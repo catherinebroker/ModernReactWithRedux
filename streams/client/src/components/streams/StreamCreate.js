b1817fffdc51250679021bbc98d449ed2e18be85
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-05-19 17:48:53 +0900

    #244

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -25,13 +25,16 @@ class StreamCreate extends React.Component {
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
 
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
         <Field
           name="title"
           component={this.renderInput}
@@ -60,9 +63,9 @@ const validate = formValues => {
   return errors;
 }
 
-
-
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
