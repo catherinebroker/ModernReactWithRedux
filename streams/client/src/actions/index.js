13faeccedfeaed252f751e9eade06505bb687fb5
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-05-22 17:02:14 +0900

    #261

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import createBrowserHistory from '../history';
 
 import {
   SIGN_IN,
@@ -28,7 +29,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  // navigation
+  createBrowserHistory.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
