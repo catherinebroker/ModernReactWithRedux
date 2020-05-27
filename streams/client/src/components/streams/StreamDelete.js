b2bade7204c2f0c497916a28a97fae264ba01fdd
Catherine Broker <39732189+catherinebroker@users.noreply.github.com>
2020-05-27 15:12:05 +0900

    #283

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,26 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Modal from '../Modal';
 import createBrowserHistory from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button pink">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-  return (
-    <div>
-      StreamDelete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={() => createBrowserHistory.push('/')}
-      />
-    </div>
-  );
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button pink">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream?"
+          actions={this.renderActions()}
+          onDismiss={() => createBrowserHistory.push('/')}
+        />
+      </div>
+    );
+  }
 }
 
-export default StreamDelete;
+export default connect(
+  null,
+  {
+    fetchStream,
+    deleteStream
+  }
+)(StreamDelete);
